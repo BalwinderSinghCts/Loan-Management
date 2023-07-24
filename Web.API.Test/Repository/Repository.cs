139025784d51c4b43e210dda0709eb77186361d8
Balwinder.singhNTG <balwinder.singh@ntgfreight.com>
2023-07-25 00:28:22 +0530

    Commit test project

---
+++ Web.API.Test/Repository/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.API.Test.NewFolder
+{
+    internal class Repository
+    {
+        [Test]
+        public void Test() {
+        Assert.True(true);
+        }
+    }
+}
